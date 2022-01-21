1d0ffadc0cdc7cdb5a5f78d65e9324c40b8536ab
kent.chen <kent.chen@hwacom.com>
2022-01-21 16:24:47 +0800

    create-new.sh add version 11

--- create-new.sh
+++ create-new.sh
@@ -2,4 +2,5 @@
 
 mvn archetype:generate \
   -DarchetypeGroupId="com.microsoft.azure" \
-  -DarchetypeArtifactId="azure-functions-archetype"
+  -DarchetypeArtifactId="azure-functions-archetype" \
+  -DjavaVersion=11
